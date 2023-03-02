fbbf7b4844b01d9031c24e0aec676c3ad9a5eb93
Andrew Lohman <andrewjlohman@gmail.com>
2023-03-02 13:40:55 -0700

    Fun with buttons!

--- public/main.js
+++ public/main.js
@@ -1,7 +1,25 @@
-const button = document.querySelector('button')
+const button1 = document.querySelector('#dontPress1')
+const messageContainer = document.querySelector('#messageContainer')
+const BC1 = document.querySelector('#buttonContainer1')
 
-const alert = evt => {
-    alert('Ducks!')
+
+const warning1 = evt => {
+    button1.style.visibility = 'hidden';
+    const button2 = document.createElement('button')
+    button2.innerHTML = 'Dont do it!';
+    BC1.appendChild(button2)
+
+    axios.get('/warning')
+    .then(response =>{
+        createList(response.data)
+    }).catch(err => console.log(err))
 }
 
-button.addEventListener('click', alert)
+const createList = (str) => {
+    const li = document.createElement('li')
+    li.innerHTML = str
+    messageContainer.appendChild(li)
+    
+}
+
+button1.addEventListener('click', warning1)
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
-
-
-
 const app = express()
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'e9eb93f322174f30814c7a1cfd702437',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+app.get('/warning', (req,res) => {
+try{
+    rollbar.log('They Pressed it!!!')
+    res.status(200).send('I told you not to press that!')
+} catch(err) {
+    console.log(err)
+}
+})
 
 
 
