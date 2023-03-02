03d935622a34b7f6a001057661afa256ef4e6d64
Andrew Lohman <andrewjlohman@gmail.com>
2023-03-02 14:41:04 -0700

    added functionality

--- public/main.js
+++ public/main.js
@@ -1,13 +1,16 @@
 const button1 = document.querySelector('#dontPress1')
+const button2 = document.querySelector('#dontPress2')
+const button3 = document.querySelector('#dontPress3')
+const button4 = document.querySelector('#dontPress4')
+const button5 = document.querySelector('#dontPress5')
+const button6 = document.querySelector('#dontPress6')
 const messageContainer = document.querySelector('#messageContainer')
-const BC1 = document.querySelector('#buttonContainer1')
+
 
 
 const warning1 = evt => {
     button1.style.visibility = 'hidden';
-    const button2 = document.createElement('button')
-    button2.innerHTML = 'Dont do it!';
-    BC1.appendChild(button2)
+    button2.style.visibility = 'visible';
 
     axios.get('/warning')
     .then(response =>{
@@ -15,7 +18,57 @@ const warning1 = evt => {
     }).catch(err => console.log(err))
 }
 
+const warning2 = evt => {
+    button2.style.visibility = 'hidden';
+    button3.style.visibility = 'visible';
+
+    axios.get('/warnin2')
+    .then(response =>{
+        createList(response.data)
+    }).catch(err => console.log(err))
+}
+
+const warning3 = evt => {
+    button3.style.visibility = 'hidden';
+    button4.style.visibility = 'visible';
+
+    axios.get('/warnin3')
+    .then(response =>{
+        createList(response.data)
+    }).catch(err => console.log(err))
+}
+
+const warning4 = evt => {
+    button4.style.visibility = 'hidden';
+    button5.style.visibility = 'visible';
+
+    axios.get('/warnin4')
+    .then(response =>{
+        createList(response.data)
+    }).catch(err => console.log(err))
+}
+
+const warning5 = evt => {
+    button5.style.visibility = 'hidden';
+    button6.style.visibility = 'visible';
+
+    axios.get('/warnin5')
+    .then(response =>{
+        createList(response.data)
+    }).catch(err => console.log(err))
+}
+
+const warning6 = evt => {
+    button6.style.visibility = 'hidden';
+    
+    axios.get('/warnin6')
+    .then(response =>{
+        createList(response.data)
+    }).catch(err => console.log(err))
+}
+
 const createList = (str) => {
+    messageContainer.innerHTML = ''
     const li = document.createElement('li')
     li.innerHTML = str
     messageContainer.appendChild(li)
@@ -23,3 +76,8 @@ const createList = (str) => {
 }
 
 button1.addEventListener('click', warning1)
+button2.addEventListener('click', warning2)
+button3.addEventListener('click', warning3)
+button4.addEventListener('click', warning4)
+button5.addEventListener('click', warning5)
+button6.addEventListener('click', warning6)
--- server/server.js
+++ server/server.js
@@ -16,7 +16,49 @@ try{
 }
 })
 
+app.get('/warnin2', (req,res) => {
+try{
+    rollbar.log('They Pressed it AGAIN!!!')
+    res.status(200).send('Why do you insist on doing the exact opposite of what i say?!')
+} catch(err) {
+    console.log(err)
+}
+})
 
+app.get('/warnin3', (req,res) => {
+    try{
+        rollbar.log('This is annoying...')
+        res.status(200).send('Okay... not funny anymore...')
+    } catch(err) {
+        console.log(err)
+    }
+    })
+app.get('/warnin4', (req,res) => {
+    try{
+        rollbar.log('Please send help:(')
+        res.status(200).send("I dont know why I thought that would've Worked...")
+    } catch(err) {
+        console.log(err)
+    }
+    })
+        
+app.get('/warnin5', (req,res) => {
+    try{
+        rollbar.log('They wont stop...')
+        res.status(200).send("OK pal... Here's a button for you! Press it! Go ahead! Do it!")
+    } catch(err) {
+        console.log(err)
+    }
+    }) 
+        
+app.get('/warnin6', (req,res) => {
+    try{
+        rollbar.log('I took care of it!')
+        res.status(200).send('WHAT NOW PUNK')
+    } catch(err) {
+        console.log(err)
+    }
+    })
 
 app.use(express.static(`${__dirname}../../public`))
 
